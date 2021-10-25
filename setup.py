8d9298d75a02496e2587f41f1c8261f546931588
J. Alexander Curtis <jalexandercurtis@gmail.com>
2021-10-25 10:15:54 -0600

    init: initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from os import name
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Alex Curtis',
+    author_email='jacurtis@hey.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jacurtis/pgbackup',
+    packages=find_packages('src')
+)
